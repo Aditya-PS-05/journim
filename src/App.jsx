094aa8af40ef0fe78b9f7c50e6a04548d289f190
Aditya-PS-05 <adityapratapsjnhh7654@gmail.com>
2025-02-06 02:51:03 +0530

    started second section

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,19 @@ import HomePage from './components/HomePage'
 
 function App() {
   return (
-    <div className='h-screen'>
-      <HomePage />
+    <div className="relative w-screen m-0 p-0 overflow-hidden box-content bg-black">
+      {/* Background Image */}
+      <img 
+        src="/home-bg.svg" 
+        alt="" 
+        className="absolute inset-0 w-full h-full object-cover m-0 p-0"
+        aria-hidden="true"
+      />
+      
+      {/* Content */}
+      <div className="relative z-10 h-full m-0 p-0">
+        <HomePage />
+      </div>
     </div>
   )
 }
